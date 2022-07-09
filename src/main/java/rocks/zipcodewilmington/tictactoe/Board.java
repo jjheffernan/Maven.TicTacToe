b6b73b9ebea3cc06f0f2b36f12a657634f287ec7
jjheffernan <jjheffernan99@gmail.com>
2022-07-09 01:00:47 -0400

    initialized HashMaps for scanning tic-tac-toe board.

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -1,10 +1,25 @@
 package rocks.zipcodewilmington.tictactoe;
 
+import java.util.*;
+
 /**
  * @author leon on 6/22/18.
  */
 public class Board {
     public Board(Character[][] matrix) {
+
+    int n = 3;
+    Map<Integer, Map<Character, Integer>> rows = new HashMap<>();
+    Map<Integer, Map<Character, Integer>> cols = new HashMap<>();
+    Map<Character, Integer> rDiagonal = new HashMap<>();
+    Map<Character, Integer> lDiagonal = new HashMap<>();
+
+    for (int i = 0; i <matrix.length; i++) {
+        int x = matrix[i][0];
+        int y = matrix[0][i];
+
+        // char move = i % 2;
+    }
     }
 
     public Boolean isInFavorOfX() {
